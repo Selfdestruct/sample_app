46ad05babe3afb3988a32c39fab50e68372adcce
Ryan Zschau <rzschau@gmail.com>
2011-05-15 21:07:01 +0300

    Can now destroy users (as admins)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110401202701
+# Schema version: 20110515162428
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 
