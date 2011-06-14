2396c0d288d132ffc43c82d5cbbc736a5258eed2
Ryan Zschau <rzschau@gmail.com>
2011-06-14 17:31:50 +0300

    Micropost Validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe Micropost do
-  before(:each) do
-    @user = Factory(:user)
-    @attr = { :content => "lorem ipsum" }
-  end
+  describe Micropost do
+    before(:each) do
+      @user = Factory(:user)
+      @attr = { :content => "lorem ipsum" }
+    end
   
-  it "should create a new instance with valid attributes" do
+    it "should create a new instance with valid attributes" do
     @user.microposts.create!(@attr)
-  end
+    end
   
   describe "user associations" do
     
@@ -23,6 +23,22 @@ describe Micropost do
     it "should have the right associated user" do
       @micropost.user_id.should == @user.id
       @micropost.user.should == @user
+    end
+  end
+
+    describe "validations" do
+      
+      it "should have a user id" do
+        Micropost.new(@attr).should_not be_valid
+      end
+      
+      it "should require nonblank content" do
+      @user.microposts.build(:content => "     ").should_not be_valid  
+      end
+    
+      it "should reject long content" do
+      @user.microposts.build(:content => "a" * 141).should_not be_valid 
+        
     end
   end
 end
